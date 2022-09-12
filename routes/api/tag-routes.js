e59ce8be3ab2216ce644ef7c8af6032afe02339d
velazquez13 <alinamvelazquez@gmail.com>
2022-09-11 21:48:34 -0700

    tag set up

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -5,24 +5,29 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all tags
+  Tag.findAll({})
   // be sure to include its associated Product data
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Tag.findOne({})
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create({})
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update({})
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.delete({})
 });
 
 module.exports = router;
